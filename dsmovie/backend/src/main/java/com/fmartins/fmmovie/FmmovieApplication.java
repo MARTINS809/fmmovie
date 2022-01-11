8b5a9a965c36900aae57c149183035b723e3a1cc
Felipe Martins <fmartins809@gmail.com>
2022-01-11 01:35:11 -0300

    Project created

---
+++ dsmovie/backend/src/main/java/com/fmartins/fmmovie/FmmovieApplication.java
@@ -0,0 +1,13 @@
+package com.fmartins.fmmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmmovieApplication.class, args);
+	}
+
+}
---
+++ dsmovie/backend/src/test/java/com/fmartins/fmmovie/FmmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmartins.fmmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
