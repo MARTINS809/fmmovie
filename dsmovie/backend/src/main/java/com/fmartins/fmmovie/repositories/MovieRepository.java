8796fe25df1a5ba95c49af384dc3a9f45cfd6028
Felipe Martins <fmartins809@gmail.com>
2022-01-16 02:16:34 -0300

    Find movies

---
+++ dsmovie/backend/src/main/java/com/fmartins/fmmovie/repositories/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.fmartins.fmmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fmartins.fmmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+	
+	
+	
+	
+
+}
